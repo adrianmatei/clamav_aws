94fd90b70f7c0e291d5c40df87619f1f1c2a7515
adrianmatei <adrian.m.matei@gmail.com>
2021-02-22 14:03:21 +0200

    Add changes

--- clamav.js
+++ clamav.js
@@ -1,7 +1,9 @@
 const AWS = require('aws-sdk');
 const fs = require('fs');
+const utils = require('./utils');
 const path = require('path');
 const constants = require('./constants');
+const execSync = require('child_process').execSync;
 
 const S3 = new AWS.S3();
 
@@ -10,7 +12,7 @@ async function downloadAVDefinitions() {
         return new Promise((resolve, reject) => {
             let destinationFile = path.join('/tmp/', filenameToDownload);
 
-            console.log('Downloading' + filenameToDownload + 'from S3 to' + destinationFile);
+            console.log('Downloading ' + filenameToDownload + 'from S3 to' + destinationFile);
 
             let localFileWriteStream = fs.createWriteStream(destinationFile);
 
@@ -36,7 +38,27 @@ async function downloadAVDefinitions() {
     return await Promise.all(downloadPromises);
 }
 
+function scanLocalFile(pathToFile) {
+    try {
+        let result = execSync(`${constants.PATH_TO_CLAMAV} -v -a --stdout -d /tmp/ '/tmp/download/${pathToFile}'`);
+
+        utils.generateSystemMessage('SUCCESSFUL SCAN, FILE CLEAN');
+
+        return constants.STATUS_CLEAN_FILE;
+    } catch(err) {
+        // Error status 1 means that the file is infected.
+        if (err.status === 1) {
+            utils.generateSystemMessage('SUCCESSFUL SCAN, FILE INFECTED');
+            return constants.STATUS_INFECTED_FILE;
+        } else {
+            utils.generateSystemMessage('-- SCAN FAILED --');
+            console.log(err);
+            return constants.STATUS_ERROR_PROCESSING_FILE;
+        }
+    }
+}
 
 module.exports = {
     downloadAVDefinitions: downloadAVDefinitions,
+    scanLocalFile: scanLocalFile
 };
--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const constants = require('./constants');
 const clamav = require('./clamav');
+const utils = require('./utils');
 const path = require('path');
+const fs = require('fs');
+
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
 
 exports.handler = (event, context) => {
-    // dependencies
-    const AWS = require('aws-sdk');
-    const s3 = new AWS.S3();
-
     var timestamp = new Date().getTime().toString();
 
     var bucket = event.Records[0].s3.bucket.name;
@@ -32,7 +33,7 @@ exports.handler = (event, context) => {
     };
 
     // perform file scan
-    let virusScanStatus = scanLocalFile(pathToFile);
+    let virusScanStatus = scanLocalFile(event, pathToFile);
 
     console.log("Scan result: " + virusScanStatus);
 
@@ -47,8 +48,44 @@ exports.handler = (event, context) => {
     return context.logStreamName
 };
 
-async function scanLocalFile(pathToFile) {
+async function scanLocalFile(event, pathToFile) {
+    let s3ObjectKey = utils.extractKeyFromS3Event(event);
+    let s3ObjectBucket = utils.extractBucketFromS3Event(event);
+
     await clamav.downloadAVDefinitions(constants.CLAMAV_BUCKET_NAME, constants.PATH_TO_AV_DEFINITIONS);
 
+    await downloadFileFromS3(s3ObjectKey, s3ObjectBucket);
+
+    let virusScanStatus = clamav.scanLocalFile(path.basename(s3ObjectKey));
+
+    utils.generateSystemMessage(`Scan status: ${virusScanStatus}`);
+
     return 'SAFE';
+}
+
+function downloadFileFromS3(s3ObjectKey, s3ObjectBucket) {
+    const downloadDir = `/tmp/download`;
+    if (!fs.existsSync(downloadDir)){
+        fs.mkdirSync(downloadDir);
+    }
+    let localPath = `${downloadDir}/${path.basename(s3ObjectKey)}`;
+
+    let writeStream = fs.createWriteStream(localPath);
+
+    utils.generateSystemMessage(`Downloading file s3://${s3ObjectBucket}/${s3ObjectKey}`);
+
+    let options = {
+        Bucket: s3ObjectBucket,
+        Key   : s3ObjectKey,
+    };
+
+    return new Promise((resolve, reject) => {
+        s3.getObject(options).createReadStream().on('end', function () {
+            utils.generateSystemMessage(`Finished downloading new object ${s3ObjectKey}`);
+            resolve();
+        }).on('error', function (err) {
+            console.log(err);
+            reject();
+        }).pipe(writeStream);
+    });
 }
