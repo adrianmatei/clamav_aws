7d95d0a10362057fb12bfb95d5d3fb41ab78109f
Matei Adrian <adrian.m.matei@gmail.com>
2021-02-23 15:54:40 +0200

    Comment tagging

--- index.js
+++ index.js
@@ -37,10 +37,10 @@ exports.handler = (event, context) => {
 
     console.log("Scan result: " + virusScanStatus);
 
-    s3.putObjectTagging(params, function(err, data) {
-        if (err) console.log(err, err.stack); // an error occurred
-        else     console.log(data);           // successful response
-    });
+//     s3.putObjectTagging(params, function(err, data) {
+//         if (err) console.log(err, err.stack); // an error occurred
+//         else     console.log(data);           // successful response
+//     });
 
 
     console.log("------ logging -------");
